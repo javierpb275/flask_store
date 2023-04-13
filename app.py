407b7d38c4eb9c1142e61a78daedbe92c11636a2
javierpb275 <javierpb275@gmail.com>
2023-04-13 20:21:34 +0200

    create store endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -18,6 +18,14 @@ stores = [
 @app.get("/api/stores")
 def get_stores():
     return {"stores": stores}
+
+
+@app.post("/api/stores")
+def create_store():
+    body = request.get_json()
+    new_store = {"name": body["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201
 
 
 app.run()
