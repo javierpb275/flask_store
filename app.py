4943d0ffefbb84afe3992efcf1b0fe277130ae0d
javierpb275 <javierpb275@gmail.com>
2023-04-13 20:10:57 +0200

    get stores endpoint

--- app.py
+++ app.py
@@ -1,3 +1,23 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+stores = [
+    {
+        "name": "My Store",
+        "items": [
+            {
+                "name": "Chair",
+                "price": 15.99
+            }
+        ]
+    }
+]
+
+
+@app.get("/api/stores")
+def get_stores():
+    return {"stores": stores}
+
+
+app.run()
