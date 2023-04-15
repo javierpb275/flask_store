885f76285c64d7349a753fc775841f441595af49
javierpb275 <javierpb275@gmail.com>
2023-04-15 16:15:54 +0200

    get store and add item to store endpoints

--- app.py
+++ app.py
@@ -17,7 +17,15 @@ stores = [
 
 @app.get("/api/stores")
 def get_stores():
-    return {"stores": stores}
+    return {"stores": stores}, 200
+
+
+@app.get("/api/stores/<string:store_name>")
+def get_store(store_name):
+    for store in stores:
+        if store["name"] == store_name:
+            return store, 200
+    return {"message": "Store Not Found"}, 404
 
 
 @app.post("/api/stores")
@@ -26,6 +34,20 @@ def create_store():
     new_store = {"name": body["name"], "items": []}
     stores.append(new_store)
     return new_store, 201
+
+
+@app.post("/api/items/<string:store_name>")
+def add_item(store_name):
+    body = request.get_json()
+    for store in stores:
+        if store["name"] == store_name:
+            new_item = {
+                "name": body["name"],
+                "price": body["price"]
+            }
+            store["items"].append(new_item)
+            return store, 201
+    return {"message": "Store Not Found"}, 404
 
 
 app.run()
