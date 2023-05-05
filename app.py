f83ed3427415c0fb248afac22481ec4caa87dc1e
javierpb275 <javierpb275@gmail.com>
2023-05-05 19:24:40 +0200

    installed flask sqlalchemy and created models

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_smorest import Api
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
 
@@ -12,6 +15,9 @@ app.config["OPENAPI_VERSION"] = "3.0.3"
 app.config["OPENAPI_URL_PREFIX"] = "/api"
 app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
 app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+
+db.init_app(app)
 
 api = Api(app)
 
