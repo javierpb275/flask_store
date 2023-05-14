c975d28c635bd7ba8311fa1314142517aed7d873
javierpb275 <javierpb275@gmail.com>
2023-05-14 17:08:33 +0200

    protected endpoints by requiring a jwt

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 
@@ -31,6 +31,33 @@ def create_app(db_url=None):
 
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "177566318276633058097699254444718673452")
     jwt = JWTManager(app)
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return(
+            jsonify({
+                "message": "The token has expired.",
+                "error": "token_expired"
+            }), 401
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return(
+            jsonify({
+                "message": "Signature verification failed.",
+                "error": "invalid_token"
+            }), 401
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return(
+            jsonify({
+                "message": "Request does not contain an access token.",
+                "error": "authorization_required"
+            }), 401
+        )
 
     with app.app_context():
         db.create_all()
--- resources/item.py
+++ resources/item.py
@@ -1,6 +1,7 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+from flask_jwt_extended import jwt_required
 
 from db import db
 from models import ItemModel
@@ -12,17 +13,20 @@ blp = Blueprint("items", __name__, description="Operations on items")
 
 @blp.route("/api/items/<int:item_id>")
 class Item(MethodView):
+
     @blp.response(200, ItemSchema)
     def get(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
         return item
 
+    @jwt_required()
     def delete(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
         db.session.delete(item)
         db.session.commit()
         return {"message": "Item deleted"}, 200
 
+    @jwt_required()
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, body, item_id):
@@ -39,10 +43,12 @@ class Item(MethodView):
 
 @blp.route("/api/items")
 class ItemList(MethodView):
+
     @blp.response(200, ItemSchema(many=True))
     def get(self):
         return ItemModel.query.all()
 
+    @jwt_required()
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, body):
