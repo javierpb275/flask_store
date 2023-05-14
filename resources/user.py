6f1073f453e6214c6b46645f9a4775da5e829d2f
javierpb275 <javierpb275@gmail.com>
2023-05-14 19:25:45 +0200

    Token refreshing with Flask-JWT-Extended

--- resources/user.py
+++ resources/user.py
@@ -2,7 +2,7 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
 from sqlalchemy.exc import SQLAlchemyError
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt, create_refresh_token, get_jwt_identity
 
 from blocklist import BLOCKLIST
 from db import db
@@ -31,9 +31,21 @@ class UserLogin(MethodView):
     def post(self, body):
         user = UserModel.query.filter(UserModel.username == body["username"]).first()
         if user and pbkdf2_sha256.verify(body["password"], user.password):
-            access_token = create_access_token(identity=user.id)
-            return {"access_token": access_token}, 200
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(identity=user.id)
+            return {"access_token": access_token, "refresh_token": refresh_token}, 200
         abort(401, message="Invalid credentials.")
+
+
+@blp.route("/api/users/refresh")
+class TokenRefresh(MethodView):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
+        return {"access_token": new_token}
 
 
 @blp.route("/api/users/logout")
